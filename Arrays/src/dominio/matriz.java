134ac92754004e2ec1c45072a540518f4c58b643
Marcosss2 <91058545+Marcosss2@users.noreply.github.com>
2021-10-04 12:07:27 +0200

    nuevo

---
+++ Arrays/src/dominio/matriz.java
@@ -0,0 +1,33 @@
+
+package dominio;
+
+
+public class matriz {
+  
+    
+     public static void main(String[] args) {
+         
+       int edades [] [] = new int[3][2];
+       edades[0][0] = 5;
+       edades[0][1] = 10;
+       edades[1][0] = 15;
+       edades[1][1] = 7;
+       imprimir(edades);
+       
+       
+     }
+       public static void imprimir(int matriz[][]) {
+       for (int i= 0; i < matriz.length; i++){
+           for (int j= 0; i < matriz[i].length; i++){
+           
+              System.out.println("edades" + i + "g" + j + "re"  + matriz[i][j]);
+           }
+           }
+       }
+       
+         
+     
+     
+    
+    
+}
