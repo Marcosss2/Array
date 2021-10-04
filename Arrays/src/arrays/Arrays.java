4aa519437d1f01fdcf746986c427330af8bfbc30
Marcosss2 <91058545+Marcosss2@users.noreply.github.com>
2021-10-04 11:48:17 +0200

    nuevo

---
+++ Arrays/src/arrays/Arrays.java
@@ -0,0 +1,41 @@
+
+package arrays;
+
+import dominio.*;
+import java.util.Scanner;
+
+
+
+public class Arrays {
+
+   
+    public static void main(String[] args) {
+     
+        Scanner lectura = new Scanner(System.in);
+        
+        Persona personas[] = new Persona[3];
+         
+        //Persona personas[] = {new Persona("Santi"),new Persona("iker"),new Persona("maria")};
+
+        //personas[0] = new Persona("Marcos");
+       //personas[1] = new Persona("raul");
+        //personas[2] = new Persona("juan");
+        
+        for (int i= 0; i < personas.length; i++){
+            
+            personas[i] = new Persona();
+            
+            
+            System.out.println("introduce nombre");
+            personas[1].setNombre(lectura.nextLine());
+        
+            System.out.println("personas [" + i + " ] ->" 
+                + personas[i].getNombre());
+       
+        
+        }
+        
+        
+    }
+    
+}
